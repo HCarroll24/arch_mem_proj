e6984dc2a5d34d210a8a21f2bfa62eef6008ec2f
HCarroll-vb <carrollh@msoe.edu>
2025-12-24 22:35:25 -0600

    file creation and start

---
+++ memory_latency_profiler.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+ * @file memory_latency_profiler.cpp
+ * @author Hunter Carroll
+ * @date 2025-12-24
+ * @version 1.0
+ *
+ * @brief This file contains the implementation of a memory latency profiler
+ * for the raspberry pi 5 that allocates memory buffers, uses a controlled
+ * access pattern, and measure the average memory latency. Sweeps from 1kb to
+ * 1gb by powers of 2.
+ * @note This file is part of the Computer Arhitecture Memory Project
+******************************************************************************/
+#include <iostream>
+#include <chrono>
+#include <vector>
+
+/**
+ * @brief main function
+ * @return 0 if successful
+*/
+int main() {
+    return 0;
+}
